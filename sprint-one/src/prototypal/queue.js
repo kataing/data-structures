9a24fc289df3251d63ce41dec805c15a4cc7104f
Kristina Taing <kataing12@gmail.com>
2019-05-29 15:43:08 -0700

    Completed prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var storage = Object.create(queueMethods);
+
+  return storage;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+	this[Object.keys(this).length + Object.keys(this)[0]] = value;
+}
+
+queueMethods.dequeue = function() {
+	var item = this[Object.keys(this)[0]];
+	delete this[Object.keys(this)[0]];
+	return item;
+}
+
+queueMethods.size = function() {
+	return Object.keys(this).length;
+}
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var storage = Object.create(stackMethods);
+
+  return storage;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+	this[Object.keys(this).length] = value;
+}
+
+stackMethods.pop = function() {
+	var item = this[Object.keys(this).length - 1];
+    delete this[Object.keys(this).length - 1];
+    return item;
+}
+
+stackMethods.size = function() {
+	return Object.keys(this).length
+}
 
 
