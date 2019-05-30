aea29fa7c5ba9f9276d624416a1b0947dcffb36d
Kristina Taing <kataing12@gmail.com>
2019-05-29 17:54:31 -0700

    Attempted es6 stack and queue (pending 1 error message)

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,4 +3,18 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
 };
 
+Queue.prototype.enqueue = function(value) {
+	this[Object.keys(this).length + Object.keys(this)[0]] = value;
+}
 
+Queue.prototype.dequeue = function() {
+	var item = this[Object.keys(this)[0]];
+	delete this[Object.keys(this)[0]];
+	return item;
+}
+
+Queue.prototype.size = function() {
+	return Object.keys(this).length;
+}
+
+var queue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,4 +3,18 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
 };
 
+Stack.prototype.push = function(value) {
+	this[Object.keys(this).length] = value;
+}
 
+Stack.prototype.pop = function() {
+	var item = this[Object.keys(this).length - 1];
+    delete this[Object.keys(this).length - 1];
+    return item;
+}
+
+Stack.prototype.size = function() {
+	return Object.keys(this).length
+}
+
+var stack = new Stack();
