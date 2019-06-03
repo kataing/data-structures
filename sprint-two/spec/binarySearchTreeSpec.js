6c1fcdeac8bbfd75e8b89012132ff69dde0af9eb
Kristina Taing <kataing12@gmail.com>
2019-06-02 17:50:08 -0700

    Added additional test to check functionality

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -37,4 +37,20 @@ describe('binarySearchTree', function() {
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5, 2, 3, 7]);
   });
+
+  // added more tests here to test the functionality
+  it('should have a working "contains" method when tree has multiple layers', function() {
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(5);
+    binarySearchTree.insert(15);
+    binarySearchTree.insert(12);
+    binarySearchTree.insert(13);
+    binarySearchTree.insert(9);
+    expect(binarySearchTree.contains(10)).to.equal(true);
+    expect(binarySearchTree.contains(5)).to.equal(true);
+    expect(binarySearchTree.contains(15)).to.equal(true);
+    expect(binarySearchTree.contains(12)).to.equal(true);
+    expect(binarySearchTree.contains(13)).to.equal(true);
+    expect(binarySearchTree.contains(9)).to.equal(true);
+  });
 });
